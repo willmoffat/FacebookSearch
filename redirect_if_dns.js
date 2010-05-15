56f3d8ee275ada82b93db3452c4cb5d22d89e0b3
Peter Burns <peter@metaweb.com>
2010-05-14 17:11:49 -0700

    include query in redirect

--- redirect_if_dns.js
+++ redirect_if_dns.js
@@ -1,2 +1,2 @@
 if (/willmoffat\.github\.com/.test(window.location.host))
-    window.location = "http://youropenbook.org/";
+    window.location = "http://youropenbook.org/" + (window.location.search || "");
